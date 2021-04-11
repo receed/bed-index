383b2dd422437bb3031198a3f708b9054226edbc
receed <efremov-343@yandex.ru>
2021-04-11 18:54:29 +0300

    Add execution time measuring

--- src/test/kotlin/BedReaderTest.kt
+++ src/test/kotlin/BedReaderTest.kt
@@ -1,11 +1,28 @@
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import java.io.File
 import java.nio.file.Path
 import kotlin.random.Random
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+import kotlin.time.nanoseconds
 
+@ExperimentalTime
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class BedReaderTest {
+    private val tempDirectory = File("tmp")
+
+    @BeforeAll
+    fun setup() {
+        tempDirectory.mkdir()
+        tempDirectory.deleteOnExit()
+    }
+
     class IndexContext(private val index: BedIndex, private val bedPath: Path) {
-        private fun findWithIndex(chromosome: String, start: Int, end: Int) =
+        fun findWithIndex(chromosome: String, start: Int, end: Int) =
             BinaryBedReader.findWithIndex(index, bedPath, chromosome, start, end)
 
         fun assertFind(chromosome: String, start: Int, end: Int, expected: List<BedEntry>) {
@@ -19,8 +36,12 @@ internal class BedReaderTest {
 
     private fun withIndex(bedPath: Path, block: IndexContext.() -> Unit) {
         val indexPath = Path.of("index")
-        BinaryBedReader.createIndex(bedPath, indexPath)
+        val indexCreationTime = measureTime { BinaryBedReader.createIndex(bedPath, indexPath) }
+        println("Index creation took $indexCreationTime")
+        val start = System.nanoTime()
         val index = BinaryBedReader.loadIndex(indexPath)
+        val loadingTime = (System.nanoTime() - start).nanoseconds
+        println("Index loading took $loadingTime")
         try {
             IndexContext(index, bedPath).block()
         } finally {
@@ -51,16 +72,25 @@ internal class BedReaderTest {
         }
     }
 
+    data class ChromosomeSpan(val chromosome: String, val start: Int, val end: Int) {
+        companion object {
+            fun random(chromosomeCount: Int, maxPosition: Int): ChromosomeSpan {
+                val chromosome = Random.nextInt(chromosomeCount).toString()
+                val (start, end) = List(2) { Random.nextInt(maxPosition) }.sorted()
+                return ChromosomeSpan(chromosome, start, end + 1)
+            }
+        }
+    }
+
     private fun generateBed(
         bedPath: Path,
         entryCount: Int,
-        maxPosition: Int = 1_000_000_000,
-        chromosomeCount: Int = 10
+        chromosomeCount: Int,
+        maxPosition: Int
     ) {
         bedPath.toFile().bufferedWriter().use { writer ->
             repeat(entryCount) {
-                val chromosome = Random.nextInt(chromosomeCount)
-                val (start, end) = List(2) { Random.nextInt(maxPosition) }.sorted()
+                val (chromosome, start, end) = ChromosomeSpan.random(chromosomeCount, maxPosition)
                 writer.appendLine("$chromosome $start ${end + 1}")
             }
         }
@@ -68,20 +98,40 @@ internal class BedReaderTest {
 
     @Test
     fun stress() {
-        val bedPath = Path.of("large.bed")
+        val bedPath = tempDirectory.toPath().resolve("large.bed")
         val entryCount = 1000
         val maxPosition = 1000
         val chromosomeCount = 10
-        generateBed(bedPath, entryCount, maxPosition, chromosomeCount)
+        generateBed(bedPath, entryCount, chromosomeCount, maxPosition)
         val simpleIndex = SimpleIndex(bedPath)
         withIndex(bedPath) {
             repeat(10000) {
-                val chromosome = Random.nextInt(chromosomeCount).toString()
-                val (start, end) = List(2) { Random.nextInt(maxPosition) }.sorted()
+                val (chromosome, start, end) = ChromosomeSpan.random(chromosomeCount, maxPosition)
                 val expected = simpleIndex.find(chromosome, start, end)
                 assertFind(chromosome, start, end, expected)
             }
         }
-        bedPath.toFile().delete()
+    }
+
+    @Test
+    fun measureFindTime() {
+        val bedPath = tempDirectory.toPath().resolve("large.bed")
+        val entryCount = 100000
+        val chromosomeCount = 10
+        val maxPosition = 1_000_000_000
+        val queries = 1000
+        val generationTime = measureTime {
+            generateBed(bedPath, entryCount, chromosomeCount, maxPosition)
+        }
+        println("Generation of the BED file took $generationTime")
+        withIndex(bedPath) {
+            val time = measureTime {
+                repeat(queries) {
+                    val (chromosome, start, end) = ChromosomeSpan.random(chromosomeCount, maxPosition)
+                    findWithIndex(chromosome, start, end)
+                }
+            }
+            println("$queries queries to an index of $entryCount entries took $time, ${time / queries} per query")
+        }
     }
 }
